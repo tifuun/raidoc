610c41418edf234e6a34316c214978392097d409
maybetree <maybetree48@proton.me>
2024-09-03 13:38:25 +0200

    Fix font awesome again

--- download-font-awesome.sh
+++ download-font-awesome.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+rm -rf fontawesome*
+rm -rf doc/fontawesome*
 wget https://use.fontawesome.com/releases/v6.6.0/fontawesome-free-6.6.0-web.zip
-unzip fontawesome-free-6.6.0-web.zip -d doc/fontawesome
+unzip fontawesome-free-6.6.0-web.zip 
+mv fontawesome-free-6.6.0-web doc/fontawesome
+
+exit 0
 
